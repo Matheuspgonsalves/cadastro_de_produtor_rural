09746f25bbbdeeebdab354c43d77ede7a1713252
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-03-28 15:54:30 -0300

    refactor: aplicar a injeção de dependÊncia no CriarProdutorUseCase

--- src/application/use-cases/produtores/criar-produtor-use-case.ts
+++ src/application/use-cases/produtores/criar-produtor-use-case.ts
@@ -1,10 +1,15 @@
 import { CriarProdutorDTO } from 'src/application/dtos/produtores/criar-produtor-dto';
 import { Cultura } from 'src/core/produtores/entities/cultura.enum';
 import { Produtor } from 'src/core/produtores/entities/Produtor.entity';
+import { ProdutorRepository } from 'src/core/produtores/repositories/produtor.repository';
 import { documentIsValid } from 'src/core/produtores/validators/documento.validator';
 
 export class CriarProdutorUseCase {
-    produtor: Produtor;
+    produtorRepository: ProdutorRepository;
+
+    constructor(produtorRepository: ProdutorRepository){
+        this.produtorRepository = produtorRepository;
+    }
 
     public execute(data: CriarProdutorDTO) {
         if(!(documentIsValid(data.cpfOuCnpj))) {
@@ -21,6 +26,8 @@ export class CriarProdutorUseCase {
             data.areaAgricultavel,
             data.areaDeVegetacao,
             data.culturasPlantadas
-        );
+        );        
+
+        this.produtorRepository.save(produtor);
     }
 }
