a54c188367ae1213a73eed01100b039b3f187c34
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-03-29 20:56:29 -0300

    feat: adicionar funcionalidade de atualização de produtor

---
+++ src/application/dtos/produtores/atualizar-produtor-dto.ts
@@ -0,0 +1,14 @@
+import { Cultura } from "src/core/produtores/entities/cultura.enum";
+
+export class AtualizarProdutorDTO {
+    cpfOuCnpj?: string;
+    nomeProdutor?: string;
+    nomeFazenda?: string;
+    cidade?: string;
+    estado?: string;
+    areaTotalHectares?: number;
+    areaAgricultavel?: number;
+    areaDeVegetacao?: number;
+    culturasPlantadas?: Cultura[];
+  }
+  
