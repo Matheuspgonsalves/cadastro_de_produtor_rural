12f12c2498e87d91de684f2e706b28b71099f7ff
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-03-29 21:32:59 -0300

    feat: criando metodo PUT e ajustes em schema

--- src/infra/http/controllers/produtores/Produtor.controller.ts
+++ src/infra/http/controllers/produtores/Produtor.controller.ts
@@ -1,16 +1,28 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put } from "@nestjs/common";
+import { AtualizarProdutorDTO } from "src/application/dtos/produtores/atualizar-produtor-dto";
 import { CriarProdutorDTO } from "src/application/dtos/produtores/criar-produtor-dto";
+import { AtualizarProdutorUseCase } from "src/application/use-cases/produtores/atualizar-produtor-use-case";
 import { CriarProdutorUseCase } from "src/application/use-cases/produtores/criar-produtor-use-case";
 
-@Controller('produtor')
+@Controller('produtores')
 export class ProdutorController {
-    constructor(private readonly criarProdutorUseCase: CriarProdutorUseCase){
+    constructor(
+        private readonly criarProdutorUseCase: CriarProdutorUseCase,
+        private readonly atualizarProdutorUseCase: AtualizarProdutorUseCase
+    ){
 
     }
 
     @Post()
     async create(@Body() body: CriarProdutorDTO){
         return this.criarProdutorUseCase.execute(body);
+    }
+
+    @Put(':cpfOuCnpj')
+    async update(
+        @Param('cpfOuCnpj') cpfOuCnpj: string, 
+        @Body() body: AtualizarProdutorDTO) {
+        return this.atualizarProdutorUseCase.execute(cpfOuCnpj, body)
     }
 
 }
