9e5724c56a85f9f392a228a4dd725002caa1197b
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-03-30 17:25:31 -0300

    fix: tentativa de resolver bug do prisma que impede os produtos de serem atualizados com o método PUT

--- prisma/produtor-prisma.repository.ts
+++ prisma/produtor-prisma.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { ProdutorRepository } from "src/core/produtores/repositories/produtor.repository";
 import { PrismaService } from "./prisma.service";
 import { Produtor } from "src/core/produtores/entities/Produtor.entity";
@@ -8,9 +8,49 @@ class ProdutorPrismaRepository implements ProdutorRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
   async save(produtor: Produtor): Promise<void> {
+
+    const produtorExistente = await this.prismaService.produtor.findFirst({
+      where: { cpfOuCnpj: produtor.cpfOuCnpj },
+    });
+    
+    if (produtorExistente) {
+      throw new Error('Produtor com este CPF/CNPJ já existe');
+    }
+    
+
     await this.prismaService.produtor.create({
       data: {
         cpfOuCnpj: produtor.cpfOuCnpj,
+        nomeProdutor: produtor.nomeProdutor,
+        nomeFazenda: produtor.nomeFazenda,
+        cidade: produtor.cidade,
+        estado: produtor.estado,
+        areaTotalHectares: produtor.areaTotalHectares,
+        areaAgricultavel: produtor.areaAgricultavel,
+        areaDeVegetacao: produtor.areaDeVegetacao,
+        culturasPlantadas: produtor.culturasPlantadas,
+      },
+    });
+  }
+
+  async update(cpfOuCnpj: string, produtor: Partial<Produtor>): Promise<void> {
+    
+    console.log('Iniciando update para CPF:', cpfOuCnpj);
+
+    const produtorExistente = await this.prismaService.produtor.findFirst({
+      where: { cpfOuCnpj },
+    });
+  
+    if (!produtorExistente) {
+      throw new HttpException('Produtor não encontrado', HttpStatus.NOT_FOUND);
+    }
+
+    console.log('Produtor encontrado?', produtorExistente);
+
+    console.log(typeof produtorExistente.id, produtorExistente.id)
+    await this.prismaService.produtor.update({
+      where: { id: produtorExistente.id },
+      data: {
         nomeProdutor: produtor.nomeProdutor,
         nomeFazenda: produtor.nomeFazenda,
         cidade: produtor.cidade,
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { ProdutorController } from './infra/http/controllers/produtores/Produtor
 import { CriarProdutorUseCase } from './application/use-cases/produtores/criar-produtor-use-case';
 import { ProdutorPrismaRepository } from './infra/database/prisma/produtor-prisma.repository';
 import { PRODUTOR_REPOSITORY, ProdutorRepository } from './core/produtores/repositories/produtor.repository';
+import { AtualizarProdutorUseCase } from './application/use-cases/produtores/atualizar-produtor-use-case';
 
 @Module({
   imports: [PrismaModule], 
   controllers: [ProdutorController], 
   providers: [
+    CriarProdutorUseCase,
+    AtualizarProdutorUseCase,
     {
       provide: PRODUTOR_REPOSITORY, 
       useClass: ProdutorPrismaRepository, 
     },
-    CriarProdutorUseCase, 
   ],
 })
 export class AppModule {}
