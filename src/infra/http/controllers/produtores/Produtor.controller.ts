2468bfa829aff4d84b4315c3759f1ecb733a7bc7
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-03-31 09:58:50 -0300

    feat: implementando o metodo LISTAR PRODUTORES

--- src/infra/http/controllers/produtores/Produtor.controller.ts
+++ src/infra/http/controllers/produtores/Produtor.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Delete, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { AtualizarProdutorDTO } from "src/application/dtos/produtores/atualizar-produtor-dto";
 import { CriarProdutorDTO } from "src/application/dtos/produtores/criar-produtor-dto";
 import { AtualizarProdutorUseCase } from "src/application/use-cases/produtores/atualizar-produtor-use-case";
 import { CriarProdutorUseCase } from "src/application/use-cases/produtores/criar-produtor-use-case";
 import { DeletarProdutorUseCase } from "src/application/use-cases/produtores/deletar-produtor-use-case";
+import { ListarProdutoresUseCase } from "src/application/use-cases/produtores/listar-produtores-use-case";
 
 @Controller('produtores')
 export class ProdutorController {
     constructor(
         private readonly criarProdutorUseCase: CriarProdutorUseCase,
         private readonly atualizarProdutorUseCase: AtualizarProdutorUseCase,
-        private readonly deletarProdutorUseCase: DeletarProdutorUseCase
+        private readonly deletarProdutorUseCase: DeletarProdutorUseCase,
+        private readonly listarProdutoresUseCase: ListarProdutoresUseCase
     ){
 
     }
@@ -31,6 +33,11 @@ export class ProdutorController {
     @Delete(':cpfOuCnpj')
         async delete(@Param('cpfOuCnpj') cpfOuCnpj: string) {
         return this.deletarProdutorUseCase.execute(cpfOuCnpj);
+    }
+
+    @Get()
+        async findAll() {
+        return this.listarProdutoresUseCase.execute();
     }
 
 }
